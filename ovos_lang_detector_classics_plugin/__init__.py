641bf6032c851deb1dd19b5addcbeda13537a979
JarbasAi <jarbasai@mailfence.com>
2024-02-23 20:34:14 +0000

    :tada:

--- ovos_lang_detector_classics_plugin/__init__.py
+++ ovos_lang_detector_classics_plugin/__init__.py
@@ -50,7 +50,6 @@ if __name__ == "__main__":
     weights = {
         "ovos-lang-detector-plugin-cld3": 0.8,
         "ovos-lang-detector-plugin-cld2": 0.8,
-        "ovos-lang-detector-plugin-lingua-podre": 1.0,
         "ovos-lang-detector-plugin-langdetect": 1.0,
         "ovos-lang-detector-plugin-fastlang": 1.0,
     }
